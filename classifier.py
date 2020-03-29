d08ccc7eba1a4ecea366bba40ccd083e8d12202d
Sushilkumar Deshmukh <sudeshmu@localhost.localdomain>
2020-03-29 11:55:03 +0530

    Bug fixes & adding a output file

--- classifier.py
+++ classifier.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Sat Mar 28 10:48:14 2020
 
-This is a temporary script file.
+@author: sudeshmu
 """
-import numpy as np
 import pandas as pd
 import pickle as pickle
 
@@ -18,11 +18,7 @@ from keras.optimizers import RMSprop
 from keras.models import Model
 from keras.callbacks import EarlyStopping
 
-dataset_file_path = "/home/sudeshmu/work/kaggle/sms-spam-collection-dataset/spam.csv"
-
-label_encoder_path = "label_encoder"
-tokenizer_sequences_path = "tokenizer_sequences"
-model_path = "model_path"
+dataset_file_path = "spam.csv"
 
 max_words = 1000
 max_len = 150
@@ -42,17 +38,7 @@ def RNN():
     model = Model(inputs=inputs,outputs=layer)
     return model
 
-def serialize_object (label_encoder, sequences, model):
-    pickle.dump (label_encoder, open (label_encoder_path, 'ab'))
-    pickle.dump (sequences, open (tokenizer_sequences_path, 'ab'))
-    pickle.dump (model,  open (model_path, 'ab'))
-
-def deserialize_object ():
-    return (pickle.load (open (label_encoder_path, 'rb')),
-            pickle.load (open (tokenizer_sequences_path, 'rb')),
-            pickle.load (open (model_path, 'rb')))
-
-def train ():
+def train_n_test ():
     data = pd.read_csv (dataset_file_path, delimiter=',', encoding='latin-1')
     X = data.v2
     y = data.v1
@@ -75,7 +61,6 @@ def train ():
     model.fit(sequences_matrix,y_train,batch_size=train_batch_size,epochs=n_epochs,
               validation_split=0.2, callbacks=[EarlyStopping(monitor='val_loss',min_delta=0.0001)])
     
-    serialize_object (label_encoder, sequences, model)
     print ("Training Completed")
     
     test_sequences = tok.texts_to_sequences(X_test)
@@ -85,13 +70,4 @@ def train ():
     print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
     
 
-def test (test_str_val):
-    (label_encoder, tokenizer_sequences, model) = deserialize_object() 
-    test_vec = tokenizer_sequences.pad_sequences (test_str_val,maxlen=max_len)
-    prediction = model.predict (test_vec)
-    return label_encoder.inverse_transform (prediction)
-    
-train ()
-
-test_str = "promotion offer message"
-print ("Prediction for text = '%s' is %s".format (test_str, test(test_str)))
+train_n_test ()
